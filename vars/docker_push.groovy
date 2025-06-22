31fd4cad06c08313bc196ca3eb9de50614ad7cd3
Ubuntu <Vishal@Kali-linux.0sw1nqd53lyuvfrnfzmtkouthd.cx.internal.cloudapp.net>
2025-06-22 06:25:36 +0000

    made changes in docker push

--- vars/docker_push.groovy
+++ vars/docker_push.groovy
@@ -1,6 +1,8 @@
 def call(String Project, String ImageTag){
   withCredentials([usernamePassword(credentialsId: 'DockerHubPat', passwordVariable: 'dockerhubpass', usernameVariable: 'dockerhubuser')]) {
-      sh "docker login -u ${dockerhubuser} -p ${dockerhubpass}"
+    sh """
+      echo "${dockerhubpass}" | docker login -u "${dockerhubuser}" --password-stdin
+      docker push "${dockerhubuser}/${Project}:${ImageTag}"
+    """
   }
-  sh "docker push ${dockerhubuser}/${Project}:${ImageTag}"
 }
