d72ba99e4b96b7c3c47c8927b2793f8a26872754
Ubuntu <Vishal@Kali-linux.0sw1nqd53lyuvfrnfzmtkouthd.cx.internal.cloudapp.net>
2025-07-06 11:49:52 +0000

    fixed errors

--- vars/code_checkout.groovy
+++ vars/code_checkout.groovy
@@ -1,12 +1,3 @@
-def call(Map config = [:]) {
-    def imageName = config.imageName ?: error("Image name is required")
-    def imageTag = config.imageTag ?: 'latest'
-    def dockerfile = config.dockerfile ?: 'Dockerfile'
-    def context = config.context ?: '.'
-    
-    echo "Building Docker image: ${imageName}:${imageTag} using ${dockerfile}"
-    
-    sh """
-        docker build -t ${imageName}:${imageTag} -t ${imageName}:latest -f ${dockerfile} ${context}
-    """
+def call(String url, String branch){
+  git url: "${url}", branch: "${branch}"
 }
